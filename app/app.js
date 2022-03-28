f2dccb0f2ba5ecc2df77518a71b63e827d607c19
osk-max <63412007+osk-max@users.noreply.github.com>
2022-03-27 20:33:07 -0500

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// String o cadena de texto
+
+let producto1 = 'Monitor de 20"';
+
+// length - Conocer la extension
+let producto2 = "Computador Core I5";
+console.log(producto1.length);
+console.log(producto2.length);
+
+// Indexof - posicion se encuentra texto a buscar
+console.log(producto1.indexOf("de"));
+console.log(producto2.indexOf("Core"));
